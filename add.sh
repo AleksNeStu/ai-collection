69814849d466eb51939a9db65b00b3b0a53b4e8c
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2024-09-06 01:43:52 +0200

    Add add

--- add.sh
+++ add.sh
@@ -1,8 +1,8 @@
 #REPO_URL="https://github.com/streamlit/llm-examples.git"
 REPO_URL="https://github.com/anthropics/courses.git"
-ROOT_NAME="streamlit"
+ROOT_NAME="anthropics"
 
-BRANCH="main"
+BRANCH="master"
 REPO_NAME=$(basename -s .git $REPO_URL)
 PREFIX="$ROOT_NAME/$REPO_NAME/sub"
 git subtree add --prefix=$PREFIX $REPO_URL $BRANCH
